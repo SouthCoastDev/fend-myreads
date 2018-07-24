20f600652c0fff68521eef42b5e83d993dcd078e
Alex <alexanderchapman@outlook.com>
2018-07-24 21:56:29 +0100

    changes for all components

--- src/MainPage.js
+++ src/MainPage.js
@@ -14,9 +14,19 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
+                        {
+                            this.props.books.filter(
+                                book => book.shelf === 'currentlyReading' 
+                            ).map(book => (
+                                <li key={book.id}>
+                                    <Book 
+                                        book = {book}
+                                        changeShelf = {this.props.changeShelf}
+                                        currentShelf = "currentlyReading"
+                                    />
+                                </li>
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
@@ -24,9 +34,17 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
+                        {
+                            this.props.books.filter(
+                                book => book.shelf === 'wantToRead' 
+                            ).map(book => (
+                                <Book 
+                                    book = {book}
+                                    changeShelf = {this.props.changeShelf}
+                                    currentShelf = "wantToRead"
+                                />
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
@@ -34,9 +52,19 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
+                    {
+                        this.props.books.filter(
+                            book => book.shelf === 'read' 
+                            ).map(book => (
+                                <li key={book.id}>
+                                    <Book 
+                                        book = {book}
+                                        changeShelf = {this.props.changeShelf}
+                                        currentShelf = "read"
+                                    />
+                                </li>
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
